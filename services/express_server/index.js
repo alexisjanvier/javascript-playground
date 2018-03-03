c76b7dc895587bb88110b781dc72eeff99a2d4b3
Alexis Janvier <contact@alexisjanvier.net>
2018-03-03 21:06:49 +0100

    Lint config

--- services/express_server/index.js
+++ services/express_server/index.js
@@ -3,5 +3,5 @@ var app = express();
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function(req, res) {
-res.send('hello world')
+    res.send('hello world');
 });
