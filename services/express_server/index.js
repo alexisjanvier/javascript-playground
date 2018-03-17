dece4407f5bc6828d1b5e05fde0bf15ea19322d5
Alexis Janvier <contact@alexisjanvier.net>
2018-03-17 18:12:33 +0100

    Lint

--- services/express_server/index.js
+++ services/express_server/index.js
@@ -1,7 +1,5 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', function(req, res) {
-    res.send('hello world');
-});
+app.get('/', (req, res) => res.send('hello world'));
